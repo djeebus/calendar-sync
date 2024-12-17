dc4ed9d5e955892ece3ba1fe8884036f2548cbee
Joe Lombrozo <joe@djeebus.net>
2024-12-17 13:08:14 -0500

    fix webhook syncing
    I didn't know wtf i was doing. now it's perfect!

--- pkg/tasks/workflows/webhookItem.go
+++ pkg/tasks/workflows/webhookItem.go
@@ -2,11 +2,10 @@ package workflows
 
 import (
 	"context"
+
 	"github.com/pkg/errors"
-	"github.com/rs/zerolog"
 
 	"calendar-sync/pkg/logs"
-	"calendar-sync/pkg/persistence"
 	"calendar-sync/pkg/tasks/activities"
 )
 
@@ -46,118 +45,44 @@ func (w *Workflows) ProcessWebhookEvent(ctx context.Context, args ProcessWebhook
 	switch args.ResourceState {
 	case "exists", "not_exists":
 		// resource was created or modified, do something!
-		return w.processEventUpsert(ctx, args, watch, args.ResourceState == "not_exists")
+		w.processInvites(ctx, watch.CalendarID)
+		w.processCopyConfigs(ctx, watch.CalendarID)
 	default:
 		log.Warn().
 			Str("state", args.ResourceState).
 			Str("uri", args.ResourceUri).
 			Msg("unknown resource state")
-		return nil
 	}
-}
-
-func (w *Workflows) processEventUpsert(
-	ctx context.Context,
-	args ProcessWebhookEventArgs,
-	watch persistence.WatchConfig,
-	isDelete bool,
-) error {
-	log := logs.GetLogger(ctx)
-
-	calendarItem, err := w.a.GetCalendarItemByItemID(ctx, activities.GetCalendarItemByItemIDArgs{
-		CalendarID: watch.CalendarID,
-		EventID:    args.ResourceID,
-	})
-	if err != nil {
-		return errors.Wrap(err, "failed to get calendar item")
-	}
-
-	copyConfigResult, err := w.a.GetCopyConfigsForSourceCalendar(ctx, activities.GetCopyConfigsForSourceCalendarArgs{
-		CalendarID: watch.CalendarID,
-	})
-	if err != nil {
-		return errors.Wrap(err, "failed to get copy configs")
-	}
-
-	w.processInvites(ctx, watch.CalendarID, calendarItem)
-
-	w.processCopyConfigs(ctx, args, watch, isDelete, copyConfigResult, log, calendarItem)
 
 	return nil
 }
 
-func (w *Workflows) processCopyConfigs(ctx context.Context, args ProcessWebhookEventArgs, watch persistence.WatchConfig, isDelete bool, copyConfigResult activities.GetCopyConfigsForSourceCalendarResult, log *zerolog.Logger, calendarItem activities.GetCalendarItemByItemIDResult) {
+func (w *Workflows) processCopyConfigs(ctx context.Context, calendarID string) {
+	log := logs.GetLogger(ctx)
+
+	copyConfigResult, err := w.a.GetCopyConfigsForSourceCalendar(ctx, activities.GetCopyConfigsForSourceCalendarArgs{
+		CalendarID: calendarID,
+	})
+	if err != nil {
+		log.Error().Err(err).Str("calendar-id", calendarID).Msg("failed to get copy configs")
+		return
+	}
+
 	for _, config := range copyConfigResult.CopyConfigs {
-		result, err := w.a.FindDestinationWebcalEvent(ctx, activities.FindWebcalEventsArgs{
-			DestinationCalendarID: config.DestinationID,
+		if err = w.CopyCalendarWorkflow(ctx, CopyCalendarWorkflowArgs{
 			SourceCalendarID:      config.SourceID,
-			SourceCalendarItemID:  args.ResourceID,
-		})
-		if err != nil {
+			DestinationCalendarID: config.DestinationID,
+		}); err != nil {
 			log.Error().
 				Err(err).
 				Str("destination-calendar-id", config.DestinationID).
 				Str("source-calendar-id", config.SourceID).
-				Str("source-calendar-item-id", args.ResourceID).
-				Msg("failed to find copied calendar events")
-			continue
-		}
-
-		// delete copied items
-		if isDelete {
-			for _, item := range result.Items {
-				if _, err := w.a.RemoveCalendarItem(ctx, activities.RemoveCalendarItemArgs{
-					CalendarID: config.DestinationID,
-					EventID:    item.Id,
-				}); err != nil {
-					log.Error().
-						Err(err).
-						Str("calendar-id", config.DestinationID).
-						Str("event-id", item.Id).
-						Msg("failed to delete calendar item")
-				}
-			}
-			continue
-		}
-
-		// create copied item
-		if len(result.Items) == 0 {
-			if _, err := w.a.CreateCalendarItem(ctx, activities.CreateCalendarItemArgs{
-				CalendarID: config.DestinationID,
-				Event:      toInsert(watch.CalendarID, calendarItem.Event),
-			}); err != nil {
-				log.Error().
-					Err(err).
-					Str("source-calendar-id", watch.CalendarID).
-					Str("destination-calendar-id", config.DestinationID).
-					Str("event-id", calendarItem.Event.Id).
-					Msg("failed to create calendar item")
-			}
-			continue
-		}
-
-		// update copied items
-		for _, item := range result.Items {
-			if patch := buildPatch(*log, *calendarItem.Event, *item); patch != nil {
-				if _, err := w.a.UpdateCalendarItem(ctx, activities.UpdateCalendarItemArgs{
-					CalendarID:     config.DestinationID,
-					CalendarItemID: item.Id,
-					Patch:          patch,
-				}); err != nil {
-					log.Error().
-						Err(err).
-						Str("source-calendar-id", watch.CalendarID).
-						Str("destination-calendar-id", config.DestinationID).
-						Str("source-event-id", calendarItem.Event.Id).
-						Str("destination-event-id", item.Id).
-						Msg("failed to update calendar item")
-				}
-			}
+				Msg("failed to copy calendar events")
 		}
 	}
 }
 
-func (w *Workflows) processInvites(ctx context.Context, calendarID string, calendarItem activities.GetCalendarItemByItemIDResult) {
+func (w *Workflows) processInvites(ctx context.Context, calendarID string) {
 	log := logs.GetLogger(ctx)
 
 	inviteConfigResult, err := w.a.GetInviteConfigsForSourceCalendar(ctx, activities.GetInviteConfigsForSourceCalendarArgs{
@@ -169,21 +94,13 @@ func (w *Workflows) processInvites(ctx context.Context, calendarID string, calen
 	}
 
 	for _, config := range inviteConfigResult.Configs {
-		if guestsContains(calendarItem.Event.Attendees, config.EmailAddress) {
-			continue
+		args := InviteCalendarWorkflowArgs{
+			CalendarID: calendarID,
+			EmailToAdd: config.EmailAddress,
 		}
-
-		inviteArgs := activities.InviteGuestArgs{
-			CalendarID:           config.CalendarID,
-			CalendarItemID:       calendarItem.Event.Id,
-			EmailAddressToInvite: config.EmailAddress,
-			Attendees:            calendarItem.Event.Attendees,
-		}
-
-		if _, err := w.a.UpdateGuestList(ctx, inviteArgs); err != nil {
+		if err = w.InviteCalendarWorkflow(ctx, args); err != nil {
 			log.Error().Err(err).
 				Str("calendar-id", config.CalendarID).
-				Str("calendar-item-id", calendarItem.Event.Id).
 				Str("email-address", config.EmailAddress).
 				Msg("failed to update guest list")
 		}
